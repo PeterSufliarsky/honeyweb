e7222bac8ff49ccde1366c47da430b2bccd0ff99
Peter Šufliarsky <3963179+PeterSufliarsky@users.noreply.github.com>
2020-08-17 19:56:25 +0200

    TTYLog details view

--- src/components/TtylogDetails.jsx
+++ src/components/TtylogDetails.jsx
@@ -9,7 +9,7 @@ class TtylogDetails extends Component {
     componentDidMount() {
         fetch(process.env.REACT_APP_API_HOST + '/sessions/' + this.props.session + '/ttylog')
         .then(res => res.json())
-        .then((data) => {
+        .then(data => {
             this.setState({ ttylog: data })
         })
         .catch(console.log)
@@ -23,16 +23,28 @@ class TtylogDetails extends Component {
                     <table className="details-section-table">
                         <thead>
                             <tr>
-                                <th>File name</th>
-                                <th>Size</th>
+                                <th className="table-column-sha256">TTY Log ID</th>
+                                <th className="table-column-shortnum">op</th>
+                                <th className="table-column-shortnum">tty</th>
+                                <th className="table-column-shortnum">length</th>
+                                <th className="table-column-shortnum">dir</th>
+                                <th className="table-column-longnum">sec</th>
+                                <th className="table-column-longnum">usec</th>
+                                <th>data</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
                                 this.state.ttylog.map((ttylog) =>
                                     <tr key={ttylog.id}>
-                                        <td>{ttylog.ttylog}</td>
-                                        <td>{ttylog.size}</td>
+                                        <td className="table-column-sha256">{ttylog.hash}</td>
+                                        <td className="table-column-shortnum">{ttylog.op}</td>
+                                        <td className="table-column-shortnum">{ttylog.tty}</td>
+                                        <td className="table-column-shortnum">{ttylog.length}</td>
+                                        <td className="table-column-shortnum">{ttylog.dir}</td>
+                                        <td className="table-column-longnum">{ttylog.sec}</td>
+                                        <td className="table-column-longnum">{ttylog.usec}</td>
+                                        <td>{ttylog.data}</td>
                                     </tr>
                                 )
                             }
